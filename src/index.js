d4381ef8f2a0e49ce0e9a26267831f6ea105c0cb
Neil Gabbadon <neil.gabbadon@emikra.com>
2016-01-31 20:44:17 -0500

    Fixes bug where uid is generated for each use of Object.assign

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
+const OBJECT_ASSIGN = 'ObjectAssign';
+
 export default function ({types: t}) {
   return {
     visitor: {
       Program: {
+        enter(path, {file}) {
+          file.set(OBJECT_ASSIGN, false);
+        },
+
         exit(path, {file, opts}) {
-          if (this.assign) {
-            const declar = t.importDeclaration([
-              t.importDefaultSpecifier(this.assign),
-            ], t.stringLiteral(opts));
-            path.node.body.unshift(declar);
+          if (!file.get(OBJECT_ASSIGN) && !path.scope.hasBinding(opts)) {
+            return;
           }
+
+          const declar = t.importDeclaration([
+            t.importDefaultSpecifier(file.get(OBJECT_ASSIGN)),
+          ], t.stringLiteral(opts));
+
+          path.node.body.unshift(declar);
         }
       },
 
       CallExpression(path, {file}) {
         if (path.get('callee').matchesPattern('Object.assign')) {
-          this.assign = path.scope.generateUidIdentifier('assign');
-          path.node.callee = this.assign;
+
+          if (!file.get(OBJECT_ASSIGN)) {
+            file.set(OBJECT_ASSIGN, path.scope.generateUidIdentifier('objectAssign'));
+          }
+
+          path.node.callee = file.get(OBJECT_ASSIGN);
         }
       }
     }
